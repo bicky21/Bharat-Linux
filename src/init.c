f6c1f761f2204ec2c75e63bf460e49c397952e40
bicky21 <bickyyadav1@hotmail.com>
2026-05-11 12:42:38 +0000

    Fix init syscall headers

--- src/init.c
+++ src/init.c
@@ -1,16 +1,37 @@
+#include <stdio.h>
 #include <unistd.h>
-
+#include <sys/mount.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 int main() {
-    const char msg[] =
-        "\n"
-        "========================\n"
-        " Bharat-linux CLI v0.1 \n"
-        "========================\n";
 
-    write(1, msg, sizeof(msg)-1);
+    mkdir("/proc", 0555);
+    mkdir("/sys", 0555);
 
-    while(1) {
-        pause();
+    mount("proc", "/proc", "proc", 0, 0);
+    mount("sysfs", "/sys", "sysfs", 0, 0);
+
+    printf("\n");
+    printf("=================================\n");
+    printf("      Bharat-linux CLI v0.1     \n");
+    printf("=================================\n");
+
+    while (1) {
+
+        char cmd[256];
+
+        printf("bharat# ");
+        fflush(stdout);
+
+        if (!fgets(cmd, sizeof(cmd), stdin))
+            continue;
+
+        if (cmd[0] == '\n')
+            continue;
+
+        system(cmd);
     }
 
     return 0;
