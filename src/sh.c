5b4d7bb885fd7d0c45a9fe2607cd3b79306d91d0
bicky21 <bickyyadav1@hotmail.com>
2026-05-12 05:23:48 +0000

    Add shell pipe support

--- src/sh.c
+++ src/sh.c
@@ -28,9 +28,95 @@ int main() {
 
             char *path = cmd + 3;
 
-            if (chdir(path) != 0) {
+            if (chdir(path) != 0)
                 printf("cd failed\n");
+
+            continue;
+        }
+
+        char *pipe_pos = strchr(cmd, '|');
+
+        if (pipe_pos != NULL) {
+
+            *pipe_pos = '\0';
+
+            char *cmd1 = cmd;
+            char *cmd2 = pipe_pos + 1;
+
+            while (*cmd2 == ' ')
+                cmd2++;
+
+            int fd[2];
+
+            pipe(fd);
+
+            pid_t p1 = fork();
+
+            if (p1 == 0) {
+
+                dup2(fd[1], 1);
+
+                close(fd[0]);
+                close(fd[1]);
+
+                char *argv1[16];
+                int argc1 = 0;
+
+                char *token = strtok(cmd1, " ");
+
+                while (token && argc1 < 15) {
+
+                    argv1[argc1++] = token;
+                    token = strtok(NULL, " ");
+                }
+
+                argv1[argc1] = NULL;
+
+                char path[256];
+
+                snprintf(path, sizeof(path), "/bin/%s", argv1[0]);
+
+                execve(path, argv1, NULL);
+
+                exit(1);
             }
+
+            pid_t p2 = fork();
+
+            if (p2 == 0) {
+
+                dup2(fd[0], 0);
+
+                close(fd[0]);
+                close(fd[1]);
+
+                char *argv2[16];
+                int argc2 = 0;
+
+                char *token = strtok(cmd2, " ");
+
+                while (token && argc2 < 15) {
+
+                    argv2[argc2++] = token;
+                    token = strtok(NULL, " ");
+                }
+
+                argv2[argc2] = NULL;
+
+                char path[256];
+
+                snprintf(path, sizeof(path), "/bin/%s", argv2[0]);
+
+                execve(path, argv2, NULL);
+
+                exit(1);
+            }
+
+            close(fd[0]);
+            close(fd[1]);
+
+            wait(NULL);
+            wait(NULL);
 
             continue;
         }
